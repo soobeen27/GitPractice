77fbcaaae1af3ec776fc031420b31a407a39f52e
soobeen27 <youdew27@gmail.com>
2024-05-30 11:58:38 +0900

    test2

--- GitPractice/ViewController.swift
+++ GitPractice/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    func testFunc() {
+        print("is there any conflict from merge?")
+    }
 
 
 }
