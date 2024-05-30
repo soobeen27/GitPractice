96d5f25a51cb4e02881d68d9507649521001f88f
soobeen27 <youdew27@gmail.com>
2024-05-30 15:45:37 +0900

    delete settings

--- GitPractice/ViewController.swift
+++ GitPractice/ViewController.swift
@@ -12,12 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("it's a first test")
-    }
-    func testFunc() {
-        print("let's make a conflict")
-    }
 
-
+    }
 }
 
